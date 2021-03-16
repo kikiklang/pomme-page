e37bb83f2f7a95536e42d121f742079de380c86b
kikiklang <kikiklang@protonmail.com>
2021-03-16 11:54:40 +0100

    new module raisin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // //////// INDEX //////// //
 
+import truncateLinkName from './raisin/raisin.js'
 import displayClock from './clock/clock.js'
 import {buttonsContainer, searchContainer, toggleEngineIcons, sendSearch} from './search/search.js'
 import {generateButtons, cryptoButtonsContainer, getBinancedata, toggleButtons} from './binance/binance.js'
 import getOpenWeatherData from './openweather/openweather.js'
 import getUnsplashData from './unsplash/unsplash.js'
 import getStormglassData from './stormglass/stormglass.js'
+
+// Raisin
+truncateLinkName()
 
 // Clock
 displayClock()
