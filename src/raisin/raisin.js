a84845023511160fe60449127f31ccb00c12ecfa
kikiklang <kikiklang@protonmail.com>
2021-03-28 21:02:44 +0200

    change raisin string break behaviour depending on raisin width

--- src/raisin/raisin.js
+++ src/raisin/raisin.js
@@ -1,8 +1,26 @@
 // //////// RAISIN //////// //
 
 function truncateLinkName() {
+  const raisinContainerWidth = document.querySelector('raisin').offsetWidth
   const raisinLinks = [...document.querySelectorAll('.raisin-link')]
-  const maxLength = 12
+  let maxLength = 0
+
+  switch (true) {
+    case (raisinContainerWidth >= 120 && raisinContainerWidth <= 123):
+      maxLength = 10
+      break
+    case (raisinContainerWidth >= 124 && raisinContainerWidth <= 131):
+      maxLength = 11
+      break
+    case (raisinContainerWidth >= 132 && raisinContainerWidth <= 135):
+      maxLength = 12
+      break
+    case (raisinContainerWidth >= 136):
+      maxLength = 13
+      break
+    default:
+      maxLength = 10
+  }
 
   raisinLinks.forEach(link => {
     if (link.innerHTML.length > maxLength) {
