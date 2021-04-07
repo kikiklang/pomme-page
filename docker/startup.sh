c659bad54f048e20efe9cf77173c93fab061cd42
Chiyo <dbodky@gmail.com>
2021-04-07 19:58:06 +0000

    Added access control

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,11 +4,6 @@ npm run build
 
 cp dist/* /usr/local/apache2/htdocs/
 
-if [ $IPRANGE ];then
-	echo "Configured access for ${IPRANGE} in /usr/local/apache2/conf/restrict_access.conf";
-	echo "<Directory /var/www/html/>" | tee /usr/local/apache2/conf/restrict_access.conf;
-	echo "  Require ip ${IPRANGE}" | tee -a /usr/local/apache2/conf/restrict_access.conf;
-	echo "</Directory>" | tee -a /usr/local/apache2/conf/restrict_access.conf;
-fi
+IPRANGE=${IPRANGE:-0.0.0.0}
 
 apachectl -D FOREGROUND
