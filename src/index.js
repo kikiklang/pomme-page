36d499eb6f968110d1566c76ad35f23a4f544bc4
kikiklang <kikiklang@protonmail.com>
2021-04-11 23:56:48 +0200

    add flip clock with date

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // //////// INDEX //////// //
 
 import truncateLinkName from './raisin/raisin.js'
-import displayClock from './clock/clock.js'
+import {clockContainer, toggleClockDisplay, displayTime, displayDate} from './clock/clock.js'
 import {buttonsContainer, searchContainer, toggleEngineIcons, sendSearch} from './search/search.js'
 import {generateButtons, cryptoButtonsContainer, getBinancedata, toggleButtons} from './binance/binance.js'
 import getOpenWeatherData from './openweather/openweather.js'
@@ -12,7 +12,9 @@ import getStormglassData from './stormglass/stormglass.js'
 truncateLinkName()
 
 // Clock
-displayClock()
+clockContainer.addEventListener('click', toggleClockDisplay)
+displayTime()
+displayDate()
 
 // Search
 buttonsContainer.addEventListener('click', toggleEngineIcons)
