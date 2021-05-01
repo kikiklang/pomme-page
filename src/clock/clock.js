7cd04ef0e3cc8effc9ecc3d96c3ebde4b6803bf7
kikiklang <kikiklang@protonmail.com>
2021-05-01 16:05:46 +0200

    fix weekday on clock for locale use

--- src/clock/clock.js
+++ src/clock/clock.js
@@ -3,24 +3,23 @@
 const clockContainer = document.querySelector('pp-clock-inner')
 
 function displayTime() {
-  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
   const date = new Date()
+  const d = date.toLocaleDateString(process.env.CLOCK_LOCALE, {weekday: 'short'}).toUpperCase()
   const h = date.getHours()
   const m = date.getMinutes()
   const s = date.getSeconds()
 
-  const day = days[date.getDay()]
   const hour = h < 10 ? `0${h}` : h
   const minute = m < 10 ? `0${m}` : m
   const second = s < 10 ? `0${s}` : s
 
-  document.querySelector('.pp-clock-time').innerHTML = `${day} : ${hour} : ${minute} : ${second}`
+  document.querySelector('.pp-clock-time').innerHTML = `${d} : ${hour} : ${minute} : ${second}`
 
   setTimeout(displayTime, 1000)
 }
 
 function displayDate() {
-  const today = new Date(Date.now())
+  const today = new Date()
   const options = {year: 'numeric', month: 'short', day: 'numeric'}
 
   document.querySelector('.pp-clock-date').innerHTML = today.toLocaleDateString(process.env.CLOCK_LOCALE, options)
