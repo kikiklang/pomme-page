a64a09524b4112225ca5e042e06525ec3cf21949
kikiklang <kikiklang@protonmail.com>
2021-05-02 10:18:40 +0200

    refactor search.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import {truncateLinkName} from './raisin/raisin.js'
 import {clockContainer, toggleClockDisplay, displayTime, displayDate} from './clock/clock.js'
-import {buttonsContainer, searchContainer, toggleEngineIcons, sendSearch} from './search/search.js'
+import {startSearchModule} from './search/search.js'
 import {generateButtons, cryptoButtonsContainer, getBinancedata, toggleButtons} from './binance/binance.js'
 import {displayOpenWeatherModule} from './openweather/openweather.js'
 import {displayUnsplashModule} from './unsplash/unsplash.js'
@@ -17,8 +17,7 @@ displayTime()
 displayDate()
 
 // Search
-buttonsContainer.addEventListener('click', toggleEngineIcons)
-searchContainer.addEventListener('keypress', sendSearch)
+startSearchModule()
 
 // Binance
 generateButtons()
--- src/search/search.js
+++ src/search/search.js
@@ -1,9 +1,24 @@
 // //////// SEARCH //////// //
 
-const searchContainer = document.querySelector('pp-search')
-const buttonsContainer = document.querySelector('pp-engine-buttons')
 const iconList = document.querySelectorAll('.pp-search-icon')
 
+/**
+ * Main exported function that attaches event handler to specified DOM elements
+ * @returns {void} Nothing
+ */
+export function startSearchModule() {
+  const searchContainer = document.querySelector('pp-search')
+  const buttonsContainer = document.querySelector('pp-engine-buttons')
+
+  buttonsContainer.addEventListener('click', toggleEngineIcons)
+  searchContainer.addEventListener('keypress', sendSearch)
+}
+
+/**
+ * Toggle icon display based on custom html attribute found through the click event
+ * @param {Object} event the event object that contains click target data
+ * @returns {void} Nothing
+ */
 function toggleEngineIcons(event) {
   iconList.forEach(icon => {
     icon.dataset.state = 'inactive'
@@ -12,6 +27,12 @@ function toggleEngineIcons(event) {
   event.target.dataset.state = 'active'
 }
 
+/**
+ * Trigger search if enter key is pressed when using the module
+ * the user picked search engine is retrieved through html custom attributes
+ * @param {*} event the event object that we are checking for a key press
+ * @returns {void} Nothing
+ */
 function sendSearch(event) {
   const input = document.querySelector('.pp-search-input')
 
@@ -23,5 +44,3 @@ function sendSearch(event) {
     input.value = ''
   }
 }
-
-export {buttonsContainer, searchContainer, toggleEngineIcons, sendSearch}
