54907a60a2fce4e275576ba0c771f7411c9772b4
kikiklang <kikiklang@protonmail.com>
2021-10-14 17:10:56 +0200

    format brackets spaces on app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 // //////// INDEX //////// //
 
-import { startMyrtilleModule } from './myrtille/myrtille.js'
-import { startRaisinModule } from './raisin/raisin.js'
-import { startClockModule } from './clock/clock.js'
-import { startSearchModule } from './search/search.js'
-import { startBinanceModule } from './binance/binance.js'
-import { startOpenWeatherModule } from './openweather/openweather.js'
-import { startUnsplashModule } from './unsplash/unsplash.js'
-import { startStormglassModule } from './stormglass/stormglass.js'
-import { startNewsapiModule } from './newsapi/newsapi.js'
+import {startMyrtilleModule} from './myrtille/myrtille.js'
+import {startRaisinModule} from './raisin/raisin.js'
+import {startClockModule} from './clock/clock.js'
+import {startSearchModule} from './search/search.js'
+import {startBinanceModule} from './binance/binance.js'
+import {startOpenWeatherModule} from './openweather/openweather.js'
+import {startUnsplashModule} from './unsplash/unsplash.js'
+import {startStormglassModule} from './stormglass/stormglass.js'
+import {startNewsapiModule} from './newsapi/newsapi.js'
 
 // Myrtille
 startMyrtilleModule()
